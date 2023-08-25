c3115adb4c7c701c0124456b75664406b69c6e76
Raspincel <raspincel@gmail.com>
2023-08-25 11:08:11 -0300

    feat: TypeScript types and interfaces

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,27 @@
+import { UseFormRegister } from "react-hook-form";
+
+export interface iLoginData {
+    email: string
+    password: string
+}
+
+export interface RegisterType {
+    [item: string]: string
+}
+
+export type UseForm = UseFormRegister<RegisterType>
+
+export interface iTool {
+    id?: number
+    title: string
+    link: string
+    description: string
+    tags: string[]
+}
+
+export interface iCreateTool {
+    title: string
+    link: string
+    description: string
+    tags: string
+}
