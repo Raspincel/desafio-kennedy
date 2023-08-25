a3f55efb69e3e3dcc5a631a814ac12310f930d2e
Raspincel <raspincel@gmail.com>
2023-08-25 11:14:51 -0300

    working application

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import MakeRoutes from "./routes";
+import { GlobalStyle } from "./styles/globalStyle";
+
+export default function App() {
+
+    return (
+        <>
+            <MakeRoutes/>
+            <GlobalStyle/>
+        </>
+    )
+}
