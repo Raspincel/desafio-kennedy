260ea16b24da82dd0c7cb219610ca9f6f1584e7a
Raspincel <raspincel@gmail.com>
2023-08-25 11:13:50 -0300

    feat: login page

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,64 @@
+import FemaquaName from '../../components/FemaquaName'
+import TextInput from '../../components/TextInput'
+import { BackgroundImage, LoginArea, LoginContainer, LoginForm, LoginWrapper } from './style'
+import { useForm } from 'react-hook-form'
+import { LoginSchema } from '../../schemas/login'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { iLoginData, UseForm } from '../../utils/interfaces'
+import { useAuthentication } from '../../hooks/contexts'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import ButtonPrimaryNeutral from '../../components/ButtonPrimaryNeutral'
+
+export default function Login() {
+    const { login, isAuthenticated } = useAuthentication()
+
+    const { register, trigger, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(LoginSchema)
+    });
+
+    const onLogin = async ({ email, password }: iLoginData)=> {
+        await trigger()
+        login({ email, password })
+    }
+
+    const navigate = useNavigate()
+    useEffect(()=> {
+        if (isAuthenticated) navigate('/dashboard')
+    }, [navigate, isAuthenticated])
+
+    return (
+        <LoginContainer>
+            <LoginArea>
+                <LoginWrapper>
+                    <FemaquaName small/>
+                    
+                    <LoginForm onSubmit={handleSubmit(onLogin)}>
+                        <TextInput 
+                            register={register as UseForm}
+                            id='email' 
+                            label='E-mail' 
+                            placeholder='Insira seu e-mail' 
+                            type='email'
+                        />
+                        {errors.email && errors.email?.message}
+                        <TextInput
+                            register={register as UseForm}
+                            id='password' 
+                            label='Senha' 
+                            placeholder='Insira sua senha' 
+                            type='password'
+                        />
+                        {errors.password && errors.password?.message}
+                        
+                        <ButtonPrimaryNeutral type="submit">
+                            Entrar
+                        </ButtonPrimaryNeutral>
+                    </LoginForm>
+                </LoginWrapper>
+            </LoginArea>
+
+            <BackgroundImage/>
+        </LoginContainer>
+    )
+}
---
+++ src/pages/Login/style.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components'
+import backgroundImage from '../../assets/images/software_login_page.jpg'
+
+export const LoginArea = styled.main`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: min(5vw, 30px);
+    margin-top: min(10vh, 40px);
+`  
+
+export const BackgroundImage = styled.div`
+    background: url(${backgroundImage}) center/cover no-repeat;;
+`
+
+export const LoginWrapper = styled.span`
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    gap: min(25vh, 150px);
+    width: min(500px, 100%);
+`
+
+export const LoginContainer = styled.div`
+    width: 100vw;
+    height: 100vh; 
+    display: grid;
+    grid-template-columns: 60% 40%;
+    
+
+    @media (max-width: 750px) {
+        grid-template-columns: 1fr;
+        ${BackgroundImage} {
+            order: 1;
+            min-height: 100px;
+            -webkit-mask-image:-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
+            mask-image: linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0));
+
+        }
+
+        ${LoginArea} {
+            order: 2;
+            margin-top: -20px;
+        }
+
+        ${LoginWrapper} {
+            gap: min(15vh, 50px);
+        }
+    }
+`
+
+
+export const LoginForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: min(25px, 5vh);
+    width: 100%;
+
+    @media (min-width: 751px) {
+        button {
+            align-self: flex-end;
+        }
+    }
+`
